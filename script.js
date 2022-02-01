c17f62c3c3499910901b8fc4a778b7e4709e4983
BrenoAndrade03 <brenopereira263@gmail.com>
2022-02-01 20:02:41 -0300

    Estrutura de dados versão 1.0

---
+++ script.js
@@ -0,0 +1,7 @@
+let nome ="Breno";
+let numero ="19";
+let ano = 2022-(numero);
+console.log(ano);
+console.log("Estou mexendo no console.log");
+let quartafeira = true;
+console.log(typeof quartafeira);
